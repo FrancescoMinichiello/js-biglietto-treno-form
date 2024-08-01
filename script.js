4f9cf06d54e8b31fe59e12d762ed908a998b3473
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-01 15:11:29 +0200

    Restart

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /*
  0 recuperare gli elementi dal DOM
+ -gestisco il cick del bottone (al click stampo "cliccato" in console)
+ -Raccolgo i campi dall'input 
  1 chiedere all'utente il numero di chilometri che vuole percorrere
  2 chiedere l'età all'utente 
  3 calcolare il prezzo del biglietto
