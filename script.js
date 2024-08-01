f3df76139603b41484c183245cee2d9d3192969e
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-01 14:31:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+ 0 recuperare gli elementi dal DOM
+ 1 chiedere all'utente il numero di chilometri che vuole percorrere
+ 2 chiedere l'età all'utente 
+ 3 calcolare il prezzo del biglietto
+ 4 applicare uno sconto (20% minorenni - 40% over 65)
+ 5 stampare in pagina
+*/
+
