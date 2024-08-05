7b468026aeb927aa471d8dc9b26fd5807b510e92
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-06 00:08:22 +0200

    Reset- added html

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
 /*
- 0 recuperare gli elementi dal DOM
- -gestisco il cick del bottone (al click stampo "cliccato" in console)
- -Raccolgo i campi dall'input 
- 1 chiedere all'utente il numero di chilometri che vuole percorrere
- 2 chiedere l'età all'utente 
- 3 calcolare il prezzo del biglietto
- 4 applicare uno sconto (20% minorenni - 40% over 65)
- 5 stampare in pagina
+1 raccolgo gli elementi dal DOM
+2 faccio reagire al click il bottone di conferma
+
+| Nel momento in cui clicco
+- recupero i valori del form
+TODO validazione
+-calcolo del prezzo di base ( in base ai kms)
+- verifico se applicare lo sconto
+- calcolo il totale definitivo
+- arrotondo a 2 decimali
+?Svuoto i campi del form?
+- preparo i valori corretti nel biglietto
+- mostro il biglietto
 */
-
-// Fase preparatoria
-
-const input = document.getElementById('km')
-console.log('km', km);
-const age = document.getElementById('age')
-console.log('age', age);
-const button = document.querySelector('button')
-console.log('button', button);
-const kmPrice = 0.21;
-console.log('kmPrice', kmPrice);
-const paragraph = document.getElementById('result')
-console.log('result', result);
-
-// Fase gestione eventi
-button.addEventListener('click', function () {
-    // Recupero il valore dell'input
-    const inputValue = km.value.trim();
-    // stampo in console
-    console.log(inputValue);
-    // stampo nel paragrafo
-    if (inputValue) {
-        paragraph.innerText = inputValue;
-        input.value = '';
-    }
-})
